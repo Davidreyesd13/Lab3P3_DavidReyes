b02344d0f39fa78ca5d47ac436acf87a561b84fe
Davidreyesd13 <davidreyesd13@gmail.com>
2023-05-05 15:18:28 -0600

    metodos

--- Lab3P3_DavidReyes.cpp
+++ Lab3P3_DavidReyes.cpp
@@ -2,18 +2,42 @@
 #include<stdlib.h>
 #include<ctime>
 using namespace std;
+int colum, fila, obstaculo;
+char** matriz{};
+void menu();
+void imprimirmatriz(char**);
+char** rellenar_matriz(int, int);
+//void agregar_obstáculos(char**, int, int, int);
 void menu() {
 	bool resp = true;
 	int opcion;
 	while (resp) {
-		cout << "1.Discos Duros\n2.Matriz Obstaculos\n0.Salir";
+		cout << "1.Discos Duros\n2.Matriz Obstaculos\n0.Salir ";
 		cin >> opcion;
 		switch (opcion) {
 		case 1:
 
 			break;
 		case 2:
-
+			cout << "Ingrese la cantidad de columnas: " << endl; 
+			cin >> colum;
+			while (colum < 5) {
+				cout << "Ingrese la cantidad de columnas: " << endl; 
+				cin >> colum;
+			}
+			cout << "Ingrese la cantidad de Filas: " << endl; 
+			cin >> fila;
+			while (fila < 5) {
+				cout << "Ingrese la cantidad de filas: " << endl; 
+				cin >> fila;
+			}
+			cout << "Ingrese la cantidad de obstaculos: " << endl;
+			cin >> obstaculo;
+			while (obstaculo<1 || obstaculo>colum * fila) {
+				cout << "Ingrese la cantidad de obstaculos: " << endl; 
+				cin >> obstaculo;
+			}
+			imprimirmatriz(rellenar_matriz(colum, fila));
 			break;
 		case 0:
 			resp = false;
@@ -23,6 +47,33 @@ void menu() {
 		}
 	}
 }
-int main(){
+void imprimirmatriz(char** matriz) {
+	for (int i = 0; i < colum; i++) {
+		for (int j = 0; j < fila; j++) {
+			cout << '"' << matriz[i][j] << '"';
+		}
+		cout << endl;
+	}
+}
+char** rellenar_matriz(int colums, int fila) {
+	char** result;
+	result = new char* [fila];
+	for (int i = 0; i < fila; i++)
+	{
+		result[i] = new char[colums];
+	}
+	for (int i = 0; i < fila; i++)
+	{
+		for (int j = 0; j < colums; j++)
+		{
+			result[i][j] = '-';
+		}
+	}
+	return result;
+}
+//void agregar_obstáculos(char** m, int obstáculos, int filas, int columnas) {
+//
+//}
+int main() {
 	menu();
 }
